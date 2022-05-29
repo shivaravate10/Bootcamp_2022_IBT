9621135bd460461405b3721c61b67c8919f17833
shiva <shiva@warrior.com>
2022-05-29 12:28:50 +0530

    changes

--- week1_warmup/linux/question1.sh
+++ week1_warmup/linux/question1.sh
@@ -1,3 +1,4 @@
+########## ls command 
 
 # Type the ls / command to list the contents of the root directory
 ls /
@@ -20,3 +21,34 @@ ls -R
 
 # Type the ls -s command (the s is lowercase) to list files or directories with their sizes:
 ls -s
+
+# To display all the files in a directory, including hidden files, enter the following command:
+
+ls –a
+
+
+############### cp command
+
+# Copy file in current directory itself.
+
+cp viewers_list.txt users_list.txt
+
+# To copy files or directories in recursive manner, use -r or -R option.
+
+cp -R newsportal archive
+
+cp -r newsportal archive
+
+cp --recursive newsportal archive
+
+
+############## Rename file using mv command 
+
+# For example, if we want to rename example1.txt into example2.txt, we would use:
+
+mv example1.txt example2.txt
+
+ls -l
+
+
+
