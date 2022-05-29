38ddb260c61df9b69c83b6562c02eaac5adeaf52
shiva <shiva@warrior.com>
2022-05-29 12:08:05 +0530

    Adding linux answer

---
+++ week1_warmup/linux/question1.sh
@@ -0,0 +1,22 @@
+
+# Type the ls / command to list the contents of the root directory
+ls /
+
+# Type the ls .. command to list the contents of the parent directory one level above. 
+ls ..
+
+# Use ls ../.. for contents two levels above:
+ls  ../..
+
+# Type the ls ~ command to list the contents in the users's home directory
+ls ~
+
+# Type the ls -d */ command to list only directories:
+ls -d */    
+
+
+# Type the ls -R command to list all files and directories with their corresponding subdirectories down to the last file
+ls -R
+
+# Type the ls -s command (the s is lowercase) to list files or directories with their sizes:
+ls -s
